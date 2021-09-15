167e07303b9d8e09bca920523da4c42122c72368
msd <13439091107@163.com>
2021-09-15 10:03:11 +0800

    打爆

--- demo02/script/webpack.config.js
+++ demo02/script/webpack.config.js
@@ -1,21 +1,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: '../src/pages/home/index.js',
+    entry: {
+        home: './src/pages/home/index.js',
+        my: './src/pages/my/index.js',
+    },
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: 'index.js'
+        filename: './js/[name]-[hash:6].js'
     },
     module: {
-
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: {
+                    loader: "url-loader",
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: "imgs/"
+                    }
+                }
+            }
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             // 常用配置
             title: "首页",   // 还需要在title标签中设置<title><%= htmlWebpackPlugin.options.title%></title>
-            template: "../template/index.html",   
+            template: "./template/index.html",   
             filename: "index.html",
             // 压缩HTML文件
             // minify: {
@@ -24,6 +39,13 @@ module.exports = {
             //     minisyCSS: true           // 压缩内联css
             // }
         }),
-      
-    ]
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        // watchOptions: {
+        //   ignored: /node_modules/
+        // },
+        host: 'localhost',
+        port: 3300
+    }
 }
